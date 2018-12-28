3336f7223798fa5031324afc35caed31d08e4a18
andrewLarsson <andrew@developersbliss.com>
2018-12-27 19:37:36 -0700

    Refactored AsssemblyExtensions and added a few new ones.

--- src/Xenox.Reflection/AssemblyExtensions.cs
+++ src/Xenox.Reflection/AssemblyExtensions.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.Loader;
 
 namespace Xenox.Reflection {
 	public static class AssemblyExtensions {
-		public static Assembly ToAssembly(this string assemblyName)
-			=> ToAssembly(ToAssemblyName(assemblyName));
-
-		public static Assembly ToAssembly(this AssemblyName assemblyName)
+		public static Assembly LoadAssembly(this AssemblyName assemblyName)
 			=> Assembly.Load(assemblyName.Name);
+
+		public static Assembly LoadAssembly(this string assemblyName)
+			=> assemblyName.ToAssemblyName().LoadAssembly();
+
+		public static Assembly LoadAssemblyWithContext(this AssemblyName assemblyName, AssemblyLoadContext context)
+			=> context.LoadFromAssemblyName(assemblyName);
 
 		public static AssemblyName ToAssemblyName(this string assemblyName)
 			=> new AssemblyName(assemblyName);
@@ -22,5 +26,17 @@ namespace Xenox.Reflection {
 
 		public static IEnumerable<Type> GetTypes(this Assembly assembly, Func<Type, bool> predicate)
 			=> assembly.GetTypes().Where(predicate);
+
+		public static IEnumerable<Assembly> LoadAssemblies(this IEnumerable<AssemblyName> assemblyNames)
+			=> assemblyNames.Select(LoadAssembly);
+
+		public static IEnumerable<Assembly> LoadAssemblies(this IEnumerable<string> assemblyNames)
+			=> assemblyNames.Select(LoadAssembly);
+
+		public static IEnumerable<Assembly> LoadAssembliesWithContext(this IEnumerable<AssemblyName> assemblyNames, AssemblyLoadContext context)
+			=> assemblyNames.Select(context.LoadFromAssemblyName);
+
+		public static IEnumerable<Assembly> LoadAssembliesWithContext(this IEnumerable<string> assemblyNames, AssemblyLoadContext context)
+			=> assemblyNames.Select(ToAssemblyName).Select(context.LoadFromAssemblyName);
 	}
 }
